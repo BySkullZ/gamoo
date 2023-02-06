70d54fa4e22ae440039e69395b2bac658f8ccdf2
BySkullZ <bahrimomo061@gmail.com>
2023-02-06 16:50:10 +0100

    button fix

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import logo_navbar from "../images/logo_navbar.png"
 
 function Navbar() {
     const [user, setUser] = useState([]);
-
-    // localStorage.removeItem("userId")
 
     useEffect(() => {
         async function getData() {
@@ -35,32 +34,44 @@ function Navbar() {
                         <div className="me-auto"></div>
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" id="button" aria-current="page" href="/">Accueil</a>
+                                <Link to="/" className="text-decoration-none">
+                                    <p className="nav-link active mb-0" id="button" aria-current="page">Accueil</p>
+                                </Link>
                             </li>
                             {user.length !== 0 &&
-                            <li className="nav-item">
-                                <a className="nav-link active" id="button" aria-current="page" href="/">Notifications</a>
-                            </li>
-                            }
-                            {user.length === 0 &&
                                 <li className="nav-item">
-                                    <a className="nav-link active" id="button" aria-current="page" href="/connexion">Se connecter</a>
+                                    <Link to="/notifications" className="text-decoration-none">
+                                            <p className="nav-link active mb-0" id="button" aria-current="page">Notifications</p>
+                                        </Link>
                                 </li>
                             }
                             {user.length === 0 &&
                                 <li className="nav-item">
-                                    <a className="nav-link active" id="button" aria-current="page" href="/inscription">S'inscrire</a>
+                                    <Link to="/connexion" className="text-decoration-none">
+                                        <p className="nav-link active mb-0" id="button" aria-current="page">Se connecter</p>
+                                    </Link>
+                                </li>
+                            }
+                            {user.length === 0 &&
+                                <li className="nav-item">
+                                    <Link to="/inscription" className="text-decoration-none">
+                                        <p className="nav-link active mb-0" id="button" aria-current="page">S'inscrire</p>
+                                    </Link>
                                 </li>
                             }
                             {user.length !== 0 && <ul>
                                 <li className="menu-deroulant d-block me-4" style={{width: "50px"}}>                                
                                     <img className="pp" src={user.profil_picture_user || "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8aff023-57ed-49e6-982a-b0cdb786956e/d4z5h7j-5a6ceb7b-95c6-4cd6-8aa4-aaf759249d0b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I4YWZmMDIzLTU3ZWQtNDllNi05ODJhLWIwY2RiNzg2OTU2ZVwvZDR6NWg3ai01YTZjZWI3Yi05NWM2LTRjZDYtOGFhNC1hYWY3NTkyNDlkMGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.icigU0Tv0i34PGz2npT9Hov5HlIBjMmeDv27JmGCn74"} alt=""/>
                                     <ul className="sous-menu position-absolute translate-middle-x">
-                                        <li><a href={`/profil/${user.id_user}`}>profil</a></li>
-                                        <li><a href="pc">paramètres et confidentialité</a></li>
-                                        <li><a href="aa">aide et assistance</a></li>
-                                        <li><a href="afac">affichage et accessibilite</a></li>
-                                        <li><p onClick={logOut}>se déconecter</p></li>
+                                        <li>
+                                            <Link to={`/profil/${user.id_user}`} className="text-decoration-none text-black">
+                                                <p>Profil</p>
+                                            </Link>    
+                                        </li>
+                                        <li><p>Paramètres et confidentialité</p></li>
+                                        <li><p>Aide et assistance</p></li>
+                                        <li><p>Affichage et accessibilité</p></li>
+                                        <li><p onClick={logOut}>Se déconnecter</p></li>
                                     </ul>
                                 </li>
                             </ul>}
--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./Navbar";
 
@@ -12,7 +12,7 @@ function Profil() {
     useEffect(() => {
 		axios.get(`https://gamoo.alwaysdata.net/profil/${id}`).then(res => {setUser(res.data[0]);});
         axios.get(`https://gamoo.alwaysdata.net/medias/${id}`).then(res => {setMedias(res.data);});
-        axios.get(`http://localhost:3001/friends/${id}`).then(res => {setFriends(res.data);});
+        axios.get(`https://gamoo.alwaysdata.net/friends/${id}`).then(res => {setFriends(res.data);});
     }, [id]);
 
     if (user === undefined) {
@@ -28,8 +28,8 @@ function Profil() {
         <div>
             <Navbar/>
             <div className="text-light font-gugi">
-                <div className="profil_banner d-flex vw-100 py-5" style={{backgroundImage: `url(${user.profil_banner_user})`}}>
-                    <img className="profil_picture mx-5" src={user.profil_picture_user || "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8aff023-57ed-49e6-982a-b0cdb786956e/d4z5h7j-5a6ceb7b-95c6-4cd6-8aa4-aaf759249d0b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I4YWZmMDIzLTU3ZWQtNDllNi05ODJhLWIwY2RiNzg2OTU2ZVwvZDR6NWg3ai01YTZjZWI3Yi05NWM2LTRjZDYtOGFhNC1hYWY3NTkyNDlkMGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.icigU0Tv0i34PGz2npT9Hov5HlIBjMmeDv27JmGCn74"} alt=""/>
+                <div className="profil-banner d-flex vw-100 py-5" style={{backgroundImage: `url(${user.profil_banner_user})`}}>
+                    <img className="profil-picture mx-5" src={user.profil_picture_user || "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8aff023-57ed-49e6-982a-b0cdb786956e/d4z5h7j-5a6ceb7b-95c6-4cd6-8aa4-aaf759249d0b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I4YWZmMDIzLTU3ZWQtNDllNi05ODJhLWIwY2RiNzg2OTU2ZVwvZDR6NWg3ai01YTZjZWI3Yi05NWM2LTRjZDYtOGFhNC1hYWY3NTkyNDlkMGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.icigU0Tv0i34PGz2npT9Hov5HlIBjMmeDv27JmGCn74"} alt=""/>
                     <h1 className="text-shadow-yellow my-auto">{user.name_user}</h1>
                 </div>
                 <div className="container text-center mt-5">
@@ -42,7 +42,7 @@ function Profil() {
                                 {medias.map((media, index) => {
                                     return (
                                         <div key={index} className="col">
-                                            <img className="profil_media" src={media.url_media} alt=""></img>
+                                            <img className="profil-media" src={media.url_media} alt=""></img>
                                         </div>
                                     )
                                 })}
@@ -53,11 +53,13 @@ function Profil() {
                         </div>
                         <div className="col-md rounded-3">
                             <h2 className="bg-blue rounded-3">Liste d'amis</h2>
-                            <ul className="bg-light text-blue">
+                            <ul className="bg-light text-blue profil-friend-list ps-0">
                                 {friends.map((friend, index) => {
                                     return (
                                         <li key={index}>
-                                            <a href={parseInt(id) === friend.u1id ? `/profil/${friend.u2id}` : `/profil/${friend.u1id}`}>{parseInt(id) === friend.u1id ? `${friend.u2name}` : `${friend.u1name}`}</a>
+                                            <Link to={parseInt(id) === friend.u1id ? `/profil/${friend.u2id}` : `/profil/${friend.u1id}`} className="text-decoration-none text-align-center">
+                                                <p>{parseInt(id) === friend.u1id ? `${friend.u2name}` : `${friend.u1name}`}</p>
+                                            </Link>
                                         </li>
                                     )
                                 })}
